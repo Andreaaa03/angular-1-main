d4e65cd104118cae8757d64a56052ceb1e0dd36c
Cavalca Andrea <92914394+Andreaaa03@users.noreply.github.com>
2023-10-28 18:59:38 +0200

    .

--- src/app/elenco-lettera/elenco-lettera.component.ts
+++ src/app/elenco-lettera/elenco-lettera.component.ts
@@ -8,14 +8,34 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class ElencoLettera implements OnInit {
     drinks: singleDrink[] = [];
+    drinkAlchol: singleDrink[] = [];
+    drinkAnalchol: singleDrink[] = [];
 
     constructor(private activatedRoute: ActivatedRoute, private drinkService: DrinkService) {}
 
     lettera_scelta: string = "";
     letters: Array<string> = [];
 
-    esiste:boolean=true;
-
+    esiste: boolean = true;
+    selectFilter = {
+        tutti: "tutti",
+        alcolici: "alcolici",
+        analcolici: "analcolici",
+    };
+    selectedFilter: string = this.selectFilter.tutti;
+    selectFilterFunction(scelta: string) {
+        switch (scelta) {
+            case this.selectFilter.tutti:
+                this.selectedFilter = this.selectFilter.tutti;
+                break;
+            case this.selectFilter.alcolici:
+                this.selectedFilter = this.selectFilter.alcolici;
+                break;
+            case this.selectFilter.analcolici:
+                this.selectedFilter = this.selectFilter.analcolici;
+                break;
+        }
+    }
     ngOnInit() {
         for (let i = 97; i <= 122; i++) {
             const letter = String.fromCharCode(i);
@@ -23,10 +43,23 @@ export class ElencoLettera implements OnInit {
         }
 
         this.activatedRoute.params.subscribe((params) => {
+            this.drinkAnalchol=[];
+            this.drinkAlchol=[];
             this.lettera_scelta = params["lettera"];
             this.drinkService.getElencoDrinks(this.lettera_scelta).subscribe((dati) => {
-                if (dati && dati.drinks !== null) this.drinks = dati.drinks;
-                else this.esiste = false;
+                if (dati && dati.drinks !== null) {
+                    this.drinks = dati.drinks;
+                    console.log(this.drinks);
+                    for(let i=0; i<this.drinks.length; i++) {
+                        if(this.drinks[i].strAlcoholic == "Alcoholic"){
+                            this.drinkAlchol.push(dati.drinks[i]);
+                        }
+                        else if(this.drinks[i].strAlcoholic == "Non alcoholic"){
+                            this.drinkAnalchol.push(dati.drinks[i]);
+                        }
+                    }
+                } else 
+                    this.esiste = false;
             });
         });
     }
